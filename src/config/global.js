b82bd4e8353961ebff10edf2e444637359f7f4e1
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2024-10-10 10:24:46 -0500

    2do Avance

--- src/config/global.js
+++ src/config/global.js
@@ -139,32 +139,125 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      link: 'https://www.google.com/',
+      tema: 'El cajero',
+      referencia:
+        'Blanco, L. A. (2013). Ética integral. Bogotá, Colombia: Ecoe Ediciones',
+      tipo: 'Libro',
+      link:
+        'https://elibro-net.bdigital.sena.edu.co/es/ereader/senavirtual/69262?page=59',
     },
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      descarga: '/downloads/prueba.pdf',
+      tema: 'Leyes de protección de datos',
+      referencia: 'Oró, R. (2015). La protección de datos.',
+      tipo: 'Libro',
+      link:
+        'https://elibro-net.bdigital.sena.edu.co/es/ereader/senavirtual/57741?prev=as', // ojo tambien se puede colocar parámetro de "descarga: '/downloads/prueba.pdf'"
     },
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Texto de la definición',
+      termino: 'Caja registradora',
+      significado:
+        'Máquina sumadora para uso de comerciantes, que llena diversos cometidos, pues indica el importe de la compra al cliente, imprime un cupón justificativo del gasto, inscribe y suma el importe de las ventas, dificulta los desfalcos y permite guardar momentáneamente el dinero.',
     },
     {
-      termino: '<em>Término con extranjerismo</em>',
-      significado: 'Definición',
+      termino: 'Cajero automático',
+      significado:
+        'Máquina sumadora para uso de comerciantes, que llena diversos cometidos, pues indica el importe de la compra al cliente, imprime un cupón justificativo del gasto, inscribe y suma el importe de las ventas, dificulta los desfalcos y permite guardar momentáneamente el dinero.',
+    },
+    {
+      termino: 'Crédito',
+      significado:
+        'Cuando una transacción se acredita en su cuenta, aparece como una transacción positiva (+) y se agrega a su saldo (a diferencia de un débito, que aparece como una transacción negativa (-) y se resta de su saldo). Un ejemplo de crédito sería el dinero en efectivo que usted depositó.',
+    },
+    {
+      termino: 'Débito',
+      significado:
+        'Cuando una transacción se debita en su cuenta, aparece como una transacción negativa (-) y se resta del saldo de su cuenta corriente vinculada, a diferencia de un crédito, que aparece como una transacción positiva (+) y se agrega a su saldo. Un ejemplo de débito en su cuenta corriente es una compra con tarjeta de débito en un comercio.',
+    },
+    {
+      termino: 'Legalización',
+      significado:
+        'Documento mediante el cual se formaliza los gastos realizados con cargo al anticipo concedido y se reintegran los recursos no gastados.',
+    },
+    {
+      termino: 'Pagos por aplicación móvil',
+      significado:
+        'Esta es una aplicación (app) que te permite pagar desde tu celular sin llevar las tarjetas contigo, de forma más fácil y segura, en comercios con datafonos habilitados. Para hacer uso de esta tecnología debes descargar la aplicación en tu <em>Smartphone</em>, crear una cuenta e inscribir las tarjetas.',
+    },
+    {
+      termino: 'Reembolso de caja menor',
+      significado:
+        'Es el soporte del pago de los gastos que se han efectuado en pequeñas cantidades por conceptos tales como: comidas, pasajes, papelería, fotocopias, etc., estos pagos deben contar con las facturas o documentos equivalentes para su verificación.',
+    },
+    {
+      termino: 'Saldo disponible',
+      significado:
+        'Su saldo disponible es el dinero en su cuenta de ahorros o cuenta corriente que está actualmente a su disposición para hacer compras, extracciones, etc. Este saldo se actualiza durante el día con las transacciones que haga.',
+    },
+    {
+      termino: 'Tarjeta de crédito',
+      significado:
+        'Instrumento que permite realizar pagos sin tener dinero en efectivo. El cargo en la cuenta se produce, por lo general, al final del mes. También se puede trasladar el pago más allá de ese momento, pero esta operación conlleva el pago de intereses. Estas tarjetas también permiten retirar dinero en efectivo en los cajeros automáticos, aunque hay que pagar comisiones.',
+    },
+    {
+      termino: 'Tarjeta de débito',
+      significado:
+        'Al igual que la tarjeta de crédito, es un medio que permite realizar pagos sin tener dinero en efectivo. La diferencia es que con la tarjeta de débito el cargo en la cuenta se produce inmediatamente, con lo que si no existe disponible el pago no se podrá realizar. Estas tarjetas también permiten retirar dinero en efectivo en los cajeros automáticos, aunque normalmente con comisiones si no se utiliza la red propia de la tarjeta.',
+    },
+    {
+      termino: 'Transacción efectiva',
+      significado:
+        'Una transacción se hace efectiva una vez que se ha acreditado o debitado de una cuenta con éxito.',
+    },
+    {
+      termino: 'Transacción rechazada',
+      significado:
+        'Usted deposita un cheque emitido para usted, pero la persona que lo libró no tiene fondos suficientes para cubrirlo. En este caso, si ya se le acreditó el depósito, se lo debitaremos de su cuenta, ya que no había fondos suficientes.',
     },
   ],
   referencias: [
     {
-      referencia: '',
+      referencia:
+        'Blanco, L. A. (2013). Ética integral. Bogotá, Colombia: Ecoe Ediciones.',
+      link:
+        'https://elibro-net.bdigital.sena.edu.co/es/ereader/senavirtual/69262?page=59',
+    },
+    {
+      referencia:
+        'Block, S. (2013). Fundamentos de administración financiera. McGraw Hill Interamericana.',
       link: '',
+    },
+    {
+      referencia:
+        'Calleja, F. (2014). Contabilidad Administrativa. México: Editorial Pearson.',
+      link: '',
+    },
+    {
+      referencia:
+        'Krugman, O. (2008). Fundamentos de Economía. Barcelona. Editorial Reverté.',
+      link: '',
+    },
+    {
+      referencia:
+        'Oró, R. (2015). La protección de datos. Barcelona, Spain: Editorial UOC.',
+      link:
+        'https://elibro-net.bdigital.sena.edu.co/es/ereader/senavirtual/57741?page=12',
+    },
+    {
+      referencia:
+        'Pérez, L. (2007). Negociación Asertiva, una visión diferente de la cobranza. México. ICM División editorial.',
+      link: '',
+    },
+    {
+      referencia:
+        'Raju, J. (2010). Fijando Precios para Ganar Competitividad. Barcelona. Profit Editorial.',
+      link: '',
+    },
+    {
+      referencia:
+        'Angulo Novoa, J. C., & Maldonado Piracon, W. A. (2016). SISTEMAS DE INFORMACIÓN GERENCIAL TIPO TRANSACCIONAL PARA PYMES. Ágora, 3(3), 60–75.',
+      link: 'https://ojs.tdea.edu.co/index.php/agora/article/view/242',
     },
   ],
   creditos: [
--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -2,12 +2,12 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    p.mb-5 A continuación, se muestra un mapa conceptual con los elementos más importantes desarrollados en este componente.
+    p.mb-5 A continuación, se presenta a manera de síntesis, un esquema que articula las principales temáticas abordadas en el desarrollo del componente formativo Recursos del cajero, incluyendo las generalidades, funciones y protocolos relacionados con el cargo, así como los recursos involucrados en el puesto de pago, los medios de pago y la normativa asociada al recaudo de caja.
 
     .row.justify-content-center
       .col-lg-10.mb-5
         figure
-          img(src="@/assets/curso/temas/sintesis.svg", alt="alt")
+          img(src="@/assets/curso/temas/sintesis.svg", alt="El esquema presenta de manera general las temáticas que aborda el componente formativo Recursos del cajero, incluyendo conocimientos asociados a los medios de pago y normativa del recaudo de caja.")
       .col-auto
         a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
           .anexo__icono
