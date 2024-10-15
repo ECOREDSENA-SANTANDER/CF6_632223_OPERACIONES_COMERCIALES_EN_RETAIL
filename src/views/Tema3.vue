21d3be86e4fe07237a66e66fbb1763497a4c965c
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2024-10-15 11:08:31 -0500

    5to Avance

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -5,7 +5,111 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 3
-      h1 Titulo de primer nivel
+      h1 Medios de pago
+    p.mb-5.mt-4 Hoy en día, existen numerosos medios de pago que permiten a los clientes efectuar sus compras de manera conveniente y segura. Es importante entender los diferentes tipos y su funcionamiento para adaptarse a las preferencias y necesidades de cada cliente.
+
+    .mb-4.d-flex.ps-lg-0
+      .d-flex.flex-wrap.align-items-center.pe-3.bg-img-blue
+        h3.col.px-4.mb-0.mx-5.text-white(data-aos="zoom-in-left") Concepto y tipos
+        img.icon(src='@/assets/curso/temas/icon.svg') 
+    p.mb-5.mt-4 Los medios de pago son elementos o instrumentos que facilitan la compra, contratación o cancelación de todo tipo de deudas, bienes o servicios, ya sea de forma física o electrónica. A continuación, se presentan los tipos más comunes:
+
+    SlyderF(columnas="col-lg-6 col-xl-4")
+      .tarjeta.bggris.p-4
+        .row.justify-content-center.mb-3
+          .col-5
+            img.mt-2(src='@/assets/curso/temas/tema3/slyf1-1.png' alt='AvatarTop')
+        p.text-center.mt-4 #[b Pago en efectivo:] pagos realizados con dinero físico, con cheque bancario al portador o con algún otro medio físico similar.
+      .tarjeta.bggris.p-4
+        .row.justify-content-center.mb-3
+          .col-5
+            img.mt-2(src='@/assets/curso/temas/tema3/slyf1-2.png' alt='AvatarTop')
+        p.text-center.mt-4 Pago con tarjeta de crédito o débito.
+      .tarjeta.bggris.p-4
+        .row.justify-content-center.mb-3
+          .col-5
+            img.mt-2(src='@/assets/curso/temas/tema3/slyf1-3.png' alt='AvatarTop')
+        p.text-center.mt-4 Pago por PayPal.
+      .tarjeta.bggris.p-4
+        .row.justify-content-center.mb-3
+          .col-5
+            img.mt-2(src='@/assets/curso/temas/tema3/slyf1-4.png' alt='AvatarTop')
+        p.text-center.mt-4 Pago a través del dispositivo móvil.
+      .tarjeta.bggris.p-4
+        .row.justify-content-center.mb-3
+          .col-5
+            img.mt-2(src='@/assets/curso/temas/tema3/slyf1-5.png' alt='AvatarTop')
+        p.text-center.mt-4 Bonos.
+      .tarjeta.bggris.p-4
+        .row.justify-content-center.mb-3
+          .col-5
+            img.mt-2(src='@/assets/curso/temas/tema3/slyf1-6.png' alt='AvatarTop')
+        p.text-center.mt-4 Pagos en línea.
+
+    .row.justify-content-center.align-items-center.mb-5.mt-5
+      .col-9.col-md-7.col-lg-3
+        img(src='@/assets/curso/temas/tema3/imagen1.png', alt='')
+      .col-sm-12.col-md-12.col-lg-9.mt-4
+        .mb-4.d-flex.ps-lg-0
+          .d-flex.flex-wrap.align-items-center.pe-3.bg-img-blue
+            img.icon3(src='@/assets/curso/temas/icon.svg') 
+            h3.col.px-4.mb-0.mx-2.text-white(data-aos="zoom-in-left") Tarjetas débito y crédito
+        p Las tarjetas de crédito permiten financiar compras, pagar a plazos y realizar adquisiciones sin necesidad de desembolsar el total del pago de inmediato, siempre que no se exceda el límite concedido por el banco. Estas tarjetas ofrecen flexibilidad y conveniencia para gestionar pagos y gastos.
+        p Por otro lado, las tarjetas de débito deducen el importe directamente de la cuenta del titular, descontando el saldo disponible de manera inmediata. Esto asegura que las compras se realicen dentro de los límites de los fondos disponibles, evitando el endeudamiento.
+  
+    .row.justify-content-center.align-items-center.mb-5
+      .col-sm-12.col-md-12.col-lg-9
+        .mb-4.d-flex.ps-lg-0
+          .d-flex.flex-wrap.align-items-center.pe-3.bg-img-blue
+            h3.col.px-4.mb-0.mx-5.text-white(data-aos="zoom-in-left") Códigos de pago
+            img.icon(src='@/assets/curso/temas/icon.svg') 
+        p Los códigos de pago son números de varios dígitos que autorizan el abono de facturas a través de medios de pago electrónicos. Estos pueden incluir servicios como: Gana, Baloto, Su Chance, los mismos #[em retail] o un cajero automático. Por lo general, el código de pago electrónico se encuentra en el extremo superior derecho de la primera hoja de la factura. Estos códigos facilitan transacciones rápidas y seguras, mejorando la eficiencia en el pago de servicios y productos.
+      .col-9.col-md-7.col-lg-3
+        img(src='@/assets/curso/temas/tema3/imagen2.png', alt='')
+
+    .row.justify-content-center.align-items-center
+        .mb-4.d-flex.ps-lg-0
+          .d-flex.flex-wrap.align-items-center.pe-3.bg-img-blue
+            h3.col.px-4.mb-0.mx-5.text-white(data-aos="zoom-in-left") Otros medios de pago
+            img.icon(src='@/assets/curso/temas/icon.svg') 
+        p.mb-5 Comprender y ofrecer una variedad de medios de pago es esencial para proporcionar una experiencia de compra flexible y accesible a todos los clientes, adaptándose a sus preferencias y mejorando su satisfacción. En el #[em retail], se destacan otros medios de pago alternativos a los tradicionales, que, de igual manera, permiten acceder a la compra de un producto, adquisición de un bien o servicio, o cancelación de deudas. Estos son:
+
+    .row.justify-content-center.mb-5
+      .col-xl-4.col-lg-7.col-md-9.col-11.mb-4.mb-xl-0
+        .crd_hover_txt(data-aos="flip-left").borde-redondo
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/temas/tema3/tar1-1.png", alt="alt")
+          .crd_hover_txt--body.cambio
+            h4.mb-3.text-center Bonos
+            p.mb-0.text-center.text-white Los bonos son títulos que otorgan el derecho a recibir pagos periódicos a cambio de entregar una cantidad de dinero en el momento de su emisión. Estos títulos pueden ser emitidos por un Estado, un gobierno local, un municipio o una empresa con el objetivo de financiarse mediante deuda.
+
+      .col-xl-4.col-lg-7.col-md-9.col-11.mb-4.mb-xl-0
+        .crd_hover_txt(data-aos="flip-left").borde-redondo
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/temas/tema3/tar1-2.png", alt="alt")
+          .crd_hover_txt--body.cambio
+            h4.mb-3.text-center Puntos
+            p.mb-0.text-center.text-white Los puntos son una forma de recompensa que se acumula al comprar en una red determinada. Estos puntos pueden canjearse por productos como alimentos, viajes, actividades recreativas, ropa, calzado, accesorios, y otros en los #[em retailers] aliados o en las tiendas #[em online].
+
+      .col-xl-4.col-lg-7.col-md-9.col-11.mb-4.mb-xl-0
+        .crd_hover_txt(data-aos="flip-left").borde-redondo
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/temas/tema3/tar1-3.png", alt="alt")
+          .crd_hover_txt--body.cambio
+            h4.mb-3.text-center Medios adicionales
+            p.mb-0.text-center.text-white Existen otros medios de pago que ofrecen alternativas a los métodos tradicionales: moneda virtual (criptomonedas y otros activos digitales), contra reembolso, en línea sin necesidad directa de tarjeta, y a través de dispositivos móviles (#[em eWallet y contactless]).
+
+      p.mt-5.mb-5 Le invitamos a consultar el siguiente contenido que presenta las ventajas en el uso de pagos en línea:
+
+      .row.justify-content-center.align-items-center.mb-5
+        .col-sm-12.col-md-12.col-lg-10
+          figure
+            .video
+              iframe(iframe width="560" height="315" src="https://www.youtube.com/embed/Vmg0zJX69Qc?si=2J_B3XS6r70hk09z" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen)
+
 
 </template>
 
--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -1,11 +1,137 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-3
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 4
-      h1 Titulo de primer nivel
+      h1 Normativa del recaudo de caja
+    
+    p.mb-4.mt-5 La necesidad de control en la recepción, custodia y despacho de bienes financieros en el #[em retail] ha llevado a la transformación de procedimientos destinados a cumplir con las normas de recaudo de caja. Esto incluye la implementación de recursos físicos, materiales y humanos para asegurar la precisión y seguridad en estas operaciones.
+
+    TabsB.color-acento-contenido.mb-3
+      .py-4.py-md-5(titulo="Controles de seguridad ambiental" :icono="require('@/assets/curso/temas/tema4/tab1-1.png')")
+        .row.align-items-center.justify-content-center
+          .col-6.col-md-4.col-lg-2.mb-4.mb-md-0
+            img.p-3(src="@/assets/curso/temas/tema4/imgtab1-1.png" alt="")
+          .col-sm-12.col-md-12.col-lg-8.mb-4.mb-md-0
+            p Los controles de seguridad ambiental son registros procedimentales, administrativos, financieros y documentales que establecen las reglas a seguir para la protección del ambiente en el #[em retail]. 
+            p Estos controles aseguran que todas las operaciones se realicen de manera sostenible y segura, minimizando el impacto ambiental.
+      .py-4.py-md-5(titulo="Riesgos" :icono="require('@/assets/curso/temas/tema4/tab1-2.png')")
+        .row.align-items-center.justify-content-center
+          .col-6.col-md-4.col-lg-2.mb-4.mb-md-0
+            img.p-3(src="@/assets/curso/temas/tema4/imgtab1-2.png" alt="")
+          .col-sm-12.col-md-12.col-lg-8.mb-4.mb-md-0
+            p El término “riesgo” se refiere a la posibilidad de desgaste o daño y sus posibles consecuencias. Estos riesgos pueden afectar a personas o grupos y son el resultado de un suceso o acción. Los riesgos se presentan tanto en ámbitos domésticos como laborales y sociales, entre otros.
+            p La evaluación del riesgo implica considerar la vulnerabilidad frente a cada tipo de peligro. Es importante distinguir adecuadamente entre peligrosidad, vulnerabilidad y riesgo para gestionar adecuadamente cada situación.
+
+    separador
+    #t_4_1.titulo-segundo.mb-5
+      h2 4.1 Seguridad y salud en el trabajo
+
+    p La seguridad y salud en el trabajo se refiere a la prevención de lesiones y enfermedades causadas por las condiciones laborales, así como a la protección y promoción de la salud de los trabajadores. Para los cajeros en el punto de pago, es crucial implementar medidas específicas para garantizar su bienestar y seguridad, por ejemplo, adecuación de sistemas de vigilancia, prácticas seguras del manejo de efectivo, ergonomía, pausas activas, y un área de trabajo saludable.
+    p Garantizar un ambiente seguro para el cajero en el puesto de pago y óptimas condiciones para el cuidado de su salud, son factores que propician su productividad y satisfacción laboral.
+    p.mb-4 Consulte el video que se presenta a continuación, relacionado con las generalidades de la Seguridad y salud en el trabajo, y el proceso integral que unifica las funciones de esta área en un sistema de gestión:
+
+    .row.justify-content-center.align-items-center.mb-3
+      .col-sm-12.col-md-12.col-lg-10
+        figure
+          .video
+            iframe(iframe width="560" height="315" src="https://www.youtube.com/embed/5CF3HZdu6Bc?si=oUUrjHf1ZHBPRbNs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen)
+
+    separador
+    #t_4_1.titulo-segundo.mb-5
+      h2 4.2 Leyes de protección de datos
+
+    .row.justify-content-center.align-items-center.mb-2
+      .col-6.col-md-2.col-lg-2
+        .row.justify-content-center.align-items-center
+          .col-12.col-md-11.col-lg-8
+            img.mt-2(src="@/assets/curso/temas/tema4/imagen1.png" alt="")
+      .col-sm-12.col-md-12.col-lg-10
+        p Cuando se habla de datos personales, se hace referencia a toda la información asociada a una persona que permite su identificación. Las leyes de protección de datos están diseñadas para salvaguardar esta información, asegurando que se maneje de manera segura y respetuosa, protegiendo la privacidad de los individuos.
+
+
+  .container.tarjeta.tarjeta--blanca.bggris.borde-redondo.p-4.p-md-5.mb-3
+
+    .row.justify-content-center.align-items-center
+
+      .col-sm-12.col-md-12.col-lg-6
+        p Los datos personales son toda la información asociada una persona y que permite su identificación:
+        
+        .row.align-items-center.justify-content-center
+
+          .col-xl-4.col-lg-7.col-md-9.col-11.mb-4.mb-xl-0
+            .crd_hover_txt(data-aos="flip-left").borde-redondo.bgblanco
+              .row.align-items-center.justify-content-center
+                .col-5.col-md-3.col-lg-5
+                  img.mt-3(src="@/assets/curso/temas/tema4/icon1-1.png", alt="alt")
+              .row.align-items-center.justify-content-center
+                .col-10
+                  p.mt-2.mb-3.text-center #[b Documento de identidad]
+
+          .col-xl-4.col-lg-7.col-md-9.col-11.mb-4.mb-xl-0
+            .crd_hover_txt(data-aos="flip-left").borde-redondo.bgblanco
+              .row.align-items-center.justify-content-center
+                .col-5.col-md-3.col-lg-5
+                  img.mt-3(src="@/assets/curso/temas/tema4/icon1-2.png", alt="alt")
+              .row.align-items-center.justify-content-center
+                .col-10
+                  p.mt-3.mb-3.text-center #[b Lugar de nacimiento]
+
+          .col-xl-4.col-lg-7.col-md-9.col-11.mb-4.mb-xl-0
+            .crd_hover_txt(data-aos="flip-left").borde-redondo.bgblanco
+              .row.align-items-center.justify-content-center
+                .col-5.col-md-3.col-lg-5
+                  img.mt-2(src="@/assets/curso/temas/tema4/icon1-3.png", alt="alt")
+              .row.align-items-center.justify-content-center
+                .col-10
+                  p.mt-2.mb-3.text-center #[b Estado civil]
+
+        .row.align-items-center.justify-content-center.mt-3
+
+          .col-xl-4.col-lg-7.col-md-9.col-11.mb-4.mb-xl-0
+            .crd_hover_txt(data-aos="flip-left").borde-redondo.bgblanco
+              .row.align-items-center.justify-content-center
+                .col-5.col-md-3.col-lg-5
+                  img.mt-3(src="@/assets/curso/temas/tema4/icon1-4.png", alt="alt")
+              .row.align-items-center.justify-content-center
+                .col-10
+                  p.mt-3.mb-4.text-center #[b Edad]
+
+          .col-xl-4.col-lg-7.col-md-9.col-11.mb-4.mb-xl-0
+            .crd_hover_txt(data-aos="flip-left").borde-redondo.bgblanco
+              .row.align-items-center.justify-content-center
+                .col-5.col-md-3.col-lg-5
+                  img.mt-3(src="@/assets/curso/temas/tema4/icon1-5.png", alt="alt")
+              .row.align-items-center.justify-content-center
+                .col-10
+                  p.mt-3.mb-4.text-center #[b Lugar de residencia]
+
+          .col-xl-4.col-lg-7.col-md-9.col-11.mb-4.mb-xl-0
+            .crd_hover_txt(data-aos="flip-left").borde-redondo.bgblanco
+              .row.align-items-center.justify-content-center
+                .col-5.col-md-3.col-lg-5
+                  img.mt-3(src="@/assets/curso/temas/tema4/icon1-6.png", alt="alt")
+              .row.align-items-center.justify-content-center
+                .col-12
+                  p.mt-2.mb-3.text-center #[b Trayectoria académica, laboral, o profesional ]
+
+      .col-11.col-md-10.col-lg-6
+        img(src="@/assets/curso/temas/tema4/imagen2.png" alt="")
+
+    p.mt-4 También existe información más sensible, como el estado de salud, características físicas, ideología política y vida sexual de una persona. En el #[em retail], es común solicitar esta información por razones de estrategia de mercadeo, con el objetivo de desarrollar estrategias basadas en las preferencias de los clientes en cuanto a marcas, productos, olores y empaques, entre otros aspectos. Esta recopilación de datos permite personalizar la experiencia del cliente y mejorar la efectividad de las campañas de #[em marketing].
+
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+
+    .row.justify-content-center.align-items-center.mb-2
+      .col-lg-10
+        .cajon.naranja.p-4.mb-4
+          p Para lograr un manejo adecuado de toda esta información, es esencial solicitar la autorización del cliente comprador. La protección de datos se centra en encontrar un equilibrio entre la información que las personas desean mantener privada y la que están dispuestas a compartir públicamente. Este consentimiento asegura que el manejo de datos personales sea transparente y respetuoso con la privacidad de los individuos, permitiendo a las empresas utilizar esta información de manera ética y conforme a las leyes de protección de datos.
+
+
+
+
 
 </template>
 
